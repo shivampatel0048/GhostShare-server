d6c2e71509f2e39c56e7bb1b0de707baca6d9efa
Shivam Patel <shivampatel0048@gmail.com>
2025-02-02 23:30:13 +0530

    cors added

--- index.ts
+++ index.ts
@@ -1,16 +1,36 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
 import urlRouter from "./routes/urlRoutes";
 
+// Load environment variables
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT ?? 5000;
 
+// CORS Options
+const allowedOrigins: string[] = [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://ghost-share-two.vercel.app'
+];
+
+const corsOptions: cors.CorsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin ?? '') !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'), false);
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+};
+
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Connect to Database
@@ -20,7 +40,7 @@ connectDB();
 app.use("/", urlRouter); // All routes in urlRouter will be prefixed with "/api"
 
 // Routes
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
     res.send("Server is running");
 });
 
